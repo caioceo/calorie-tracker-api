b4eef4f21c6c4f9485766bc527fe677a96c12cd3
caioceo <caioceo06@gmail.com>
2026-01-08 19:31:43 -0300

    init spring boot

---
+++ src/main/java/dev/caio/fitsy/FitsyApplication.java
@@ -0,0 +1,13 @@
+package dev.caio.fitsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/caio/fitsy/FitsyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.caio.fitsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
