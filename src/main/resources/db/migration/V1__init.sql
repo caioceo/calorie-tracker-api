595787a19f51535d6536c2e35c93a9a7c92504d2
caioceo <caioceo06@gmail.com>
2026-01-14 01:58:41 -0300

    feat: add flyway and initial database schema
    - initial shcema
    - flyway dependency
    - hibernate.dll to validate

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,118 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users_info (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    altura FLOAT,
+    peso FLOAT,
+    sexo VARCHAR(10),
+    nivel_atividade VARCHAR(20),
+    data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    role_name VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE users_roles (
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    role_id INT REFERENCES roles(id),
+    PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE meta (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    objetivo VARCHAR(50) NOT NULL,
+    estrategia VARCHAR(50) NOT NULL,
+    peso_inicial FLOAT NOT NULL,
+    peso_meta FLOAT NOT NULL,
+    status VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE meta_nutrientes (
+    id SERIAL PRIMARY KEY,
+    meta_id INT REFERENCES meta(id) ON DELETE CASCADE,
+    data_inicio DATE NOT NULL DEFAULT CURRENT_DATE,
+    data_fim DATE,
+    status VARCHAR(20) NOT NULL,
+    calorias FLOAT,
+    proteinas FLOAT,
+    carboidratos FLOAT,
+    gorduras FLOAT,
+    fibras FLOAT,
+    colesterol FLOAT,
+    sodio FLOAT,
+    potassio FLOAT,
+    ferro FLOAT,
+    calcio FLOAT,
+    zinco FLOAT,
+    vitamina_c FLOAT,
+    magnesio FLOAT
+);
+
+CREATE TABLE historico_peso (
+    id SERIAL PRIMARY KEY,
+    user_info_id INT REFERENCES users_info(id) ON DELETE CASCADE,
+    peso FLOAT,
+    data_registro DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE categorias_alimentos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE alimentos (
+    id SERIAL PRIMARY KEY,
+    criador_id INT REFERENCES users(id),
+    categoria_id INT REFERENCES categorias_alimentos(id),
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE nutrientes (
+    alimento_id INT PRIMARY KEY REFERENCES alimentos(id) ON DELETE CASCADE,
+    calorias FLOAT,
+    proteinas FLOAT,
+    carboidratos FLOAT,
+    gorduras FLOAT,
+    fibras FLOAT,
+    colesterol FLOAT,
+    sodio FLOAT,
+    potassio FLOAT,
+    ferro FLOAT,
+    calcio FLOAT,
+    zinco FLOAT,
+    vitamina_c FLOAT,
+    magnesio FLOAT
+);
+
+CREATE TABLE diario_alimentar (
+    id SERIAL PRIMARY KEY,
+    user_info_id INT REFERENCES users_info(id) ON DELETE CASCADE,
+    meta_nutrientes_id INT NOT NULL REFERENCES meta_nutrientes(id),
+    data_registro DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE refeicoes (
+    id SERIAL PRIMARY KEY,
+    diario_alimentar_id INT REFERENCES diario_alimentar(id) ON DELETE CASCADE,
+    nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE alimento_refeicao (
+    id SERIAL PRIMARY KEY,
+    refeicao_id INT REFERENCES refeicoes(id) ON DELETE CASCADE,
+    alimento_id INT REFERENCES alimentos(id),
+    quantidade FLOAT NOT NULL,
+    calorias FLOAT,
+    proteinas FLOAT,
+    carboidratos FLOAT,
+    gorduras FLOAT
+);
