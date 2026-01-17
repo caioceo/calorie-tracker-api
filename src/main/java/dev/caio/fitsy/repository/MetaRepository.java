fd8b599d5dbfa3e1dae4759fc03a5bfa69f91930
caioceo <caioceo06@gmail.com>
2026-01-17 01:17:37 -0300

    feat(meta): add CRUD and repository methods for Meta entity
    - add POST endpoint to create a new meta
    - add PUT endpoint to update existing meta
    - add GET endpoint to fetch active meta
    - add GET endpoint to fetch all metas
    - create MetaRequest and MetaResponse DTOs
    - add MetaMapper for request/response conversion
    - add repository methods:
        - findByUserAndStatus
        - findByUserOrderByDataInicioDesc

--- src/main/java/dev/caio/fitsy/repository/MetaRepository.java
+++ src/main/java/dev/caio/fitsy/repository/MetaRepository.java
@@ -1,7 +1,16 @@
 package dev.caio.fitsy.repository;
 
 import dev.caio.fitsy.model.Meta;
+import dev.caio.fitsy.model.User;
+import dev.caio.fitsy.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MetaRepository extends JpaRepository<Meta, Long> {
+    Meta findByUserAndStatus(User user, Status status);
+
+    List<Meta> findByUserOrderByDataInicioDesc(User user);
 }
